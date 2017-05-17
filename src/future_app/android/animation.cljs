0bda3616aa5d39eaac60be81fb84d86aabe91e1d
Manuela <manuela@Ubuntu.station>
2017-05-17 15:34:24 +0200

    added ART

--- src/future_app/android/animation.cljs
+++ src/future_app/android/animation.cljs
@@ -5,6 +5,11 @@
             [future-app.subs]))
 
 (def ReactNative (js/require "react-native"))
+(def ART (.-ART ReactNative))
+(def Surface (r/adapt-react-class (.-Surface ART)))
+(def Group (r/adapt-react-class (.-Group ART)))
+(def Shape (r/adapt-react-class (.-Shape ART)))
+; (def shape* (.createAnimatedComponent Animated Shape))
 
 (def app-registry (.-AppRegistry ReactNative))
 (def text (r/adapt-react-class (.-Text ReactNative)))
@@ -16,8 +21,20 @@
 (def animated-value (.-Value Animated))
 (def fadeAnim (new animated-value 0))
 (def animatedView (r/adapt-react-class (.-View Animated)))
-(def state  #js {:fadeAnim fadeAnim})
+;(def state  #js {:fadeAnim fadeAnim})
+(def state #js {})
+(def radial-gradient (.-RadialGradient ART))
+(def create-animated-component (.-createAnimatedComponent Animated))
+(def Shape* (create-animated-component Shape) )
 
+
+(defn interpolate
+  [a-value easing]
+  (.interpolate
+    a-value
+    #js {:inputRange (array 0 1)
+         :outputRange (array 0 1)
+         :easing easing}))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;with meta-data
 ; (def FadeInView
@@ -37,6 +54,7 @@
                                     (.timing
                                      (.-fadeAnim state) #js {:toValue 1 :duration 2000})
                                     (.start)))
+    :component-will-mount (fn [] (set! (.-fadeAnim state) fadeAnim))
     :reagent-render (fn [navigator]
                       [view
                        [animatedView
@@ -45,7 +63,17 @@
                        [view  {:style {:flex-direction "column" :margin 40 :align-items "center"}}
                         [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
                                               :on-press #(.resetTo navigator #js {:id "firstpage"})}
-                         [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]]])}))
+                         [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]]
+                       [view [Surface {:width 500 :height 500}
+                              [Group {:x 100 :y 0}
+                               [Shape {:d "M 25, 1.5, A 23.5, 23.5, 0, 1, 1, 24.99, 1.5 Z" :stroke "black" :strokeWidth 1}]]]]])}))
 
+;  [view [Surface {:width 500 :height 500}
+;         [Group {:x 100 :y 0}
+;          [Shape {:d "M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80" :stroke "#2ca02c" :strokeWidth 1}]]]]
+
+
+;read this to undestand the d property of SVG
+;https://www.w3.org/TR/SVG/paths.html#PathDataCurveCommands
 (defn animation [{navigator :navigator}]
   [FadeInView navigator])
